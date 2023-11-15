0aae5b6a2e623fb12f9bd59b6366397879c62900
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-15 22:28:04 +0100

    loading info page

--- src/pages/PageInfo.tsx
+++ src/pages/PageInfo.tsx
@@ -1,7 +1,22 @@
-// import { useQuery, useMutation } from "@tanstack/react-query";
+import { useQuery, useMutation } from "@tanstack/react-query";
+import * as tools from "../tools";
+
+const _employees = [
+	{
+		firstName: "111",
+	},
+	{
+		firstName: "222",
+	},
+];
 
 export const PageInfo = () => {
-	return (
-		<p>This is the info page.</p>
-	)
-}
+	const employeesQuery = useQuery({
+		queryKey: ["employees"],
+		queryFn: () => tools.wait(1000).then(() => [..._employees]),
+	});
+
+	if (employeesQuery.isLoading) return <p>loading...</p>;
+	if (employeesQuery.isError) return <pre>{JSON.stringify(employeesQuery.error)}</pre>
+	return <p>This is the info page.</p>;
+};
